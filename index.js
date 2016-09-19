842782ea19c7609df26b659d4385d62149469c32
Robert Obreczarek <robert@obreczarek.com>
2016-09-19 19:17:00 +0200

    revamped object code to use prototype constructor syntax added console.log output to test what values are resulting from code
    outstanding issues:
    animal array push is breaking
    prototype based properties are returning function body, not values

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
-var zoo = function() {
+var Zoo = function(name) {
   var animals = [];
-  var count = 0;
 
-  this.all_animals = function () {
-    return animals;
-  }
-
-  this.animal_count = function () {
-    return animals.length;
-  };
+  this.zooName = name;
 };
 
-var animal = (function () {
-  var species_name = '';
-  var diet_type = '';
-  
-  this.species = species_name;
-  this.diet = diet_type;
+Zoo.prototype.addAnimal = function(animal) {
+  return animals.push(animal);
 };
 
-var elephant = new animal();
-elephant.species = "Elephant";
-var lion = new animal("Lion");
-lion.species = "Lion";
+Zoo.prototype.getAnimals = function() {
+  return animals;
+};
 
-var myzoo = new zoo();
-myzoo.animals.push(elephant);
-myzoo.animals.push(lion);
+Zoo.prototype.getCount = function() {
+  return animals.count;
+};
 
+var Animal = function (species) {
+  var group = '';
+  var diet = '';
 
+  this.species = species;
+};
+
+Animal.prototype.getGroup = function() {
+  return group;
+};
+
+Animal.prototype.getDiet = function() {
+  return diet;
+};
+
+var myzoo = new Zoo("My Zoo");
+
+var elephant = new Animal("Elephant");
+elephant.group = "Vertebrates";
+elephant.diet = "Herbivore";
+
+var lion = new Animal("Lion");
+lion.group = "Vertebrates";
+lion.diet = "Carnivore";
+
+myzoo.addAnimal(elephant);
+myzoo.addAnimal(lion);
+
+/*
+Test Output
+*/
+console.log( '##################################' );
+console.log( 'Welcome to ' + myzoo.zooName );
+console.log( '##################################' );
+console.log( '-----------------------------------' );
+console.log('Animal #1: ' + elephant.species);
+console.log('Group: ' + elephant.getGroup);
+console.log('Diet: ' + elephant.getDiet);
+console.log( '-----------------------------------' );
+console.log('Animal #2: ' + lion.species);
+console.log('Group: ' + lion.getGroup);
+console.log('Diet: ' + lion.getDiet);
+console.log( '-----------------------------------' );
+console.log( '##################################' );
+console.log( 'Number of animals in zoo: ' + myzoo.getCount  );
+console.log( 'First zoo animal: ' + myzoo.getAnimals[0].species  );
+console.log( 'Second zoo animal: ' + myzoo.getAnimals[1].species  );
